d7fb10436c8830d1fbfead84208bd4838f13d457
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2022-12-15 15:45:08 +0300

    API completed

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT||3000;
 
+// article array.
+let articles = [];
+
+// GET routes.
+app.get("/rent", (req, res) => {
+    axios.get("https://www.expedia.com/")
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            $('a', html).each(function() {
+                const text = $(this).text();
+                const link = $(this).attr("href");
+                if(!link.includes("https://www.expedia.com/")) {
+                    articles.push()
+                } else {
+                    articles.push({
+                        text,
+                        link
+                    })
+                }
+                
+            });
+            res.json(articles);
+        })
+        .catch((error) => { console.log(error) });
+})
+
 // initialize server.
 app.listen(PORT, () => {
     console.log(`Server Application Running on http://localhost:${PORT}`);
